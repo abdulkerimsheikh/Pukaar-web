8f8a7f4d9046e75312e0107fbb2473bebb9f0704
Abdul Kareem <karima9185432@gmail.com>
2025-10-19 15:08:52 +0500

    updated files

--- script.js
+++ script.js
@@ -322,7 +322,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./scripts/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(reg => console.log('✅ Service Worker registered:', reg.scope))
       .catch(err => console.error('❌ Service Worker failed:', err));
   });
